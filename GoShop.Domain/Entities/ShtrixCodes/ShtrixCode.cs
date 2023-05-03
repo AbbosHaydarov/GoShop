2a040efb5d8d9c771871220b8a029b6cfe10f9b0
Haydarovcoder <abboshaydarov0799@gmail.com>
2023-05-03 20:05:48 +0500

    Add project files.

---
+++ GoShop.Domain/Entities/ShtrixCodes/ShtrixCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoShop.Domain.Commons;
+
+namespace GoShop.Domain.Entities.ShtrixCodes
+{
+    public class ShtrixCode : Auditable
+    {
+        public string Code { get; set; }
+    }
+}
