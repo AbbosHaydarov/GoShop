122dbbdd612596d3694d6c77416084ae0f9b1d3a
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-05-10 11:19:24 +0500

    SERVICE: Create Category DTO

---
+++ GoShop.Service/DTOs/Categories/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoShop.Service.DTOs.Categories
+{
+    public class CategoryForCreationDTO
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
