d0f9b8ec899c76a3aef13f6d7db271155f4ff507
ShohruhUzDev <shohruh7464784@mail.ru>
2023-05-22 10:28:46 +0500

    SERVICE : Create ISoldProductService

---
+++ GoShop.Service/DTOs/SoldProducts/SoldProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace GoShop.Service.DTOs.SoldProducts
+{
+    public class SoldProductDTO
+    {
+        public int NumberOfSold { get; set; }
+        public DateTime SoldAt { get; set; }
+        public int MarketId { get; set; }
+    }
+}
