a4cffc77f864c572a1d0cb05ad250d7eec6d8811
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-05-30 18:33:13 +0500

    DOMAIN: Create User Entity

---
+++ GoShop.Domain/Commons/Enums/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoShop.Domain.Commons.Enums
+{
+    public enum UserRole
+    {
+        User,
+        Admin
+    }
+}
---
+++ GoShop.Domain/Entities/Users/User.cs
@@ -0,0 +1,14 @@
+﻿using GoShop.Domain.Commons;
+using GoShop.Domain.Commons.Enums;
+
+namespace GoShop.Domain.Entities.Users
+{
+    public class User : Auditable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
